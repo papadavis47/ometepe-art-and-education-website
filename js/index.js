a5b91abfbdf4d2f38d8fe65bc80ff78336bd5e57
John William Davis <jwdavisdeveloper@gmail.com>
2021-03-02 18:53:41 -0800

    Change background color, put form and modal window

--- js/index.js
+++ js/index.js
@@ -4,3 +4,17 @@ const navLinks = document.querySelector("#nav-links");
 burgerIcon.addEventListener("click", () => {
   navLinks.classList.toggle("is-active");
 });
+
+// Modal
+
+const signupButton = document.querySelector("#signup");
+const modalBg = document.querySelector("#modal-bg-1");
+const modal = document.querySelector("#modal-1");
+
+signupButton.addEventListener("click", (e) => {
+  modal.classList.add("is-active");
+});
+
+modalBg.addEventListener("click", (e) => {
+  modal.classList.remove("is-active");
+});
